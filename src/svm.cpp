09c2eccdedb2ab948b5feb6818f2593fc41753be
Dirk Eddelbuettel <edd@debian.org>
2010-08-23 01:49:26 +0000

    Kurt and CRAN now (well, for R 2.12.*) insist that no files .hpp be in src so renamed rshark.hpp to rshark.h

--- src/svm.cpp
+++ src/svm.cpp
@@ -20,7 +20,7 @@
 // Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 // MA 02111-1307, USA
 
-//#include <rshark.hpp>
+//#include <rshark.h>
 
 #include <Rcpp.h>
 #include <Rng/GlobalRng.h>
--- src/utils.cpp
+++ src/utils.cpp
@@ -20,4 +20,4 @@
 // Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 // MA 02111-1307, USA
 
-//#include <rshark.hpp>
+//#include <rshark.h>
